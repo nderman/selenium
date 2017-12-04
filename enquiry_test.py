efee5ca7acdba635c730d76e471003d022ae1ef9
Neal Derman <neal@travelground.com>
2017-12-04 15:33:02 +0200

    update test to use new test estab and use hour as num guests for unique hourly enquiry

--- enquiry_test.py
+++ enquiry_test.py
@@ -8,14 +8,14 @@ from selenium.webdriver.support.ui import Select
 
 import datetime
 from dateutil.relativedelta import relativedelta
-startDate = datetime.datetime.now() + relativedelta(years=2)-relativedelta(days=datetime.datetime.now().hour)
+startDate = datetime.datetime.now() + relativedelta(years=2)
 endDate = startDate + relativedelta(days=2)
 
 # Create a new instance of the Firefox driver
 driver = webdriver.PhantomJS()
 
 # go to the google home page
-driver.get("https://www.travelground.com/accommodation/karoo-ground-sister-b-bne/book?instant=no")
+driver.get("https://www.travelground.com/accommodation/karoo-ground-selenium-test/book?instant=no")
 driver.execute_script ("document.getElementById('dpd1').removeAttribute('readonly',0);");
 driver.execute_script ("document.getElementById('dpd2').removeAttribute('readonly',0);");
 # driver.execute_script ("document.getElementById('dpd1').val('15 Dec 2017');");
@@ -37,7 +37,7 @@ inputElement.send_keys(Keys.RETURN)
 select = Select(driver.find_element_by_class_name("room-select"))
 select.select_by_visible_text("1")
 select = Select(driver.find_element_by_class_name("adult_count"))
-select.select_by_visible_text("1")
+select.select_by_visible_text(str(datetime.datetime.now().hour))
 
 inputElement = driver.find_element_by_id("firstname")
 inputElement.send_keys("Test")
@@ -54,5 +54,6 @@ try:
 	WebDriverWait(driver, 10).until(EC.title_contains("Successful Enquiry"))
 	print (driver.title)
 finally:
-	driver.quit()
+	pass
+	# driver.quit()
 
